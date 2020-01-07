45a421a52aa68378ca994b457fecfda3aed67a36
App Academy Student <email@email.com>
2020-01-07 15:51:35 -0500

    dynamic width height, game class

--- src/index.js
+++ src/index.js
@@ -4,9 +4,12 @@ const App = require("./scripts/app.js");
 import Game from "./scripts/game";
 
 document.addEventListener("DOMContentLoaded", ()=>{
-  const canvas = document.getElementById("game-canvas");
-  
+  const canvas = document.getElementById("game-canvas");  
   const ctx = canvas.getContext("2d");
+  const SCREEN_WIDTH = 1200;
+  const SCREEN_HEIGHT = 800;
+  ctx.canvas.width = SCREEN_WIDTH;
+  ctx.canvas.height = SCREEN_HEIGHT;
   // App(ctx);
-  Game(ctx);
+  let game = new Game(ctx, SCREEN_WIDTH, SCREEN_HEIGHT);
 });
--- src/scripts/game.js
+++ src/scripts/game.js
@@ -1,28 +1,41 @@
 import TestObject from "./testobject";
 
-
-
-function Game(ctx){
-  const SCREEN_WIDTH = 800;
-  const SCREEN_HEIGHT = 600;
-
-  ctx.clearRect(0,0,SCREEN_WIDTH,SCREEN_HEIGHT);
-  let obj1 = new TestObject(SCREEN_WIDTH, SCREEN_HEIGHT);
-  obj1.draw(ctx);
-
-  let lastTime = 0;
-
-  function gameLoop (timestamp){
-    let dt = timestamp - lastTime;
-    lastTime = timestamp;    
-    ctx.clearRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
-    obj1.update(dt);
-    obj1.draw(ctx);
-    requestAnimationFrame(gameLoop);
+export default class Game{
+  constructor(context, width, height){
+    this.ctx = context;
+    this.screenHeight = height;
+    this.screenWidth = width;
+    this.runGame = this.runGame.bind(this)();
   }
-  gameLoop();
+
+  runGame(){
+    console.log("hello");
+    console.log(this.screenHeight);
+  }
+
+  gameLoop(){
+
+  }
+
+
 }
 
+// function Game(ctx){
+  
 
+//   ctx.clearRect(0,0,SCREEN_WIDTH,SCREEN_HEIGHT);
+//   let obj1 = new TestObject(SCREEN_WIDTH, SCREEN_HEIGHT);
+//   obj1.draw(ctx);
 
-export default Game;
+//   let lastTime = 0;
+
+//   function gameLoop (timestamp){
+//     let dt = timestamp - lastTime;
+//     lastTime = timestamp;    
+//     ctx.clearRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+//     obj1.update(dt);
+//     obj1.draw(ctx);
+//     requestAnimationFrame(gameLoop);
+//   }
+//   gameLoop();
+// }
