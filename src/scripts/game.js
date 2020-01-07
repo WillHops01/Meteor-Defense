ef327e829232988be8538648632dd884d32a3061
App Academy Student <email@email.com>
2020-01-07 16:19:59 -0500

    mouseclick event listener

--- src/scripts/game.js
+++ src/scripts/game.js
@@ -7,22 +7,33 @@ export default class Game{
     this.screenWidth = width;    
     this.meteorArray = [];
     this.elapsedTime = 0;
-
+    this.gameLoop = this.gameLoop.bind(this);
     this.runGame = this.runGame.bind(this)();
+    this.canvas = document.getElementById("game-canvas");
+    this.canvas.addEventListener("click", this.handleClick, false);
+    
+    // ${this.handleClick(event)}`);
+  }
+
+  handleClick(e){
+    console.log(`clicked ${e.offsetX}`);
   }
 
   runGame(){
-    //while not lost
-    
-    //update meteorarray
-    //redraw meteors
+    //initial setup logic
+    //then, start gameLoop
   }
 
   gameLoop(){
+    //while not lost
 
+      //update meteorarray
+      //redraw meteors
+      //listen for clicks
+      //repeat
+
+    //
   }
-
-
 }
 
 // function Game(ctx){
